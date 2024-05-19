50900337cbe838c78cf238839b1ca267cc7d48b2
Michael <michael.mingdao.li@gmail.com>
2024-05-19 15:26:55 -0700

    Adapted dual curriculum for TSP

--- options.py
+++ options.py
@@ -56,6 +56,9 @@ def get_options(args=None):
                              ' to save memory (default None means no shrinking)')
     parser.add_argument('--data_distribution', type=str, default=None,
                         help='Data distribution to use during training, defaults and options depend on problem.')
+    parser.add_argument('--edit_fn', type=str, default=None,
+                        help="Dual curriculum edit function to use: 'global_perturb', 'local_perturb' or 'random_edit'."
+                             'Defaults to no edit function.')
 
     # Misc
     parser.add_argument('--log_step', type=int, default=50, help='Log info every log_step steps')
