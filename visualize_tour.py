5fe71126aa55cb776f79f8446ff504978ffaaac1
Ansh <anshkothari558@gmail.com>
2025-08-01 04:14:52 +0530

    Create visualize_tour.py

---
+++ visualize_tour.py
@@ -0,0 +1,30 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+from utils import load_model
+
+# Load model
+model, _ = load_model("outputs/tsp_20/modelAlpha_20250731T180107/epoch-0.pt")
+model.eval()
+model.set_decode_type("greedy")
+
+# Generate 1 random instance
+dataset = model.problem.make_dataset(num_samples=1)
+sample = dataset[0]
+coords = sample['loc'].numpy()
+
+# Get tour
+with torch.no_grad():
+    tour, _ = model(sample, return_pi=True)
+
+tour = tour.squeeze().numpy()
+
+# Close the tour loop
+tour = np.append(tour, tour[0])
+
+# Plot
+plt.figure(figsize=(6, 6))
+plt.plot(coords[tour, 0], coords[tour, 1], 'o-', markersize=8)
+plt.title("TSP Tour")
+plt.grid(True)
+plt.show()
